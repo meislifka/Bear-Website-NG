7164eb64ebfabca3aa7a4fd34ccdd4f3611ddb82
meislifka <meislifka@gmail.com>
2023-08-31 20:07:51 -0400

    add seaon2 nav

--- scripts/nav.js
+++ scripts/nav.js
@@ -25,14 +25,12 @@ body {
   <nav id="nav-placeholder" class="sidenav">
     <li><a href="index.html">Home</a></li>
     <button onclick="
-      poop();
-    " 
-    class="dropdown-btn" 
-    >Season 1 >
-      <i class="fa-solid fa-caret-down" style="color: #0a0000;"></i>
+      poop('Season1','dropD-1');" 
+      class="dropdown-btn Season1" 
+      >Season 1 >
     </button>
 
-    <div id="dropD" class="dropdown-container" style="">
+    <div id="dropD-1" class="dropdown-container" style="">
       <a href="S1E1.html">1: Pilot</a>
       <a href="S1E2.html">2: Kyptonite</a>
       <a href="S1E3.html">3: Wedding</a>
@@ -58,26 +56,68 @@ body {
       <a href="S1E23.html">23: Backslide</a>
       <a href="S1E24.html">24: See Ya</a>
     </div>
+
+    <button onclick="
+    poop('Season2','dropD-2');" 
+    class="dropdown-btn Season2" 
+    >Season 2 >
+  </button>
+
+  <div id="dropD-2" class="dropdown-container" style="">
+    <a href="S2E1">1: Re-Launch</a>
+    <a href="S2E2">2: Katie</a>
+    <a href="S2E3">3: Fluffer</a>
+    <a href="S2E4">4: Neighbors</a>
+    <a href="S2E5">5: Models</a>
+    <a href="S2E6">6: Halloween</a>
+    <a href="S2E7">7: Menzies</a>
+    <a href="S2E8">8: Parents</a>
+    <a href="S2E9">9: Eggs</a>
+    <a href="S2E10">10: Bathtub</a>
+    <a href="S2E11">11: Santa</a>
+    <a href="S2E12">12: Cabin</a>
+    <a href="S2E13">13: A Father's Love</a>
+    <a href="S2E14">14: Pepperwood</a>
+    <a href="S2E15">15: Cooler</a>
+    <a href="S2E16">16: Table 34</a>
+    <a href="S2E17">17: Parking Spot</a>
+    <a href="S2E18">18: TinFinity</a>
+    <a href="S2E19">19: Quick Hardening Caulk</a>
+    <a href="S2E20">20: Chicago</a>
+    <a href="S2E21">21: First Date</a>
+    <a href="S2E22">22: Bachelorette Party</a>
+    <a href="S2E23">23: Virgins</a>
+    <a href="S2E24">24: Winston's Birthday (Part 1)</a>
+    <a href="S2E25">24: Elaine's Big Day</a>
+  </div>
+
   </nav>
 </body>
 
 </html>` + body;
 
-function poop() {
-  const buttonElement = document.querySelector('.dropdown-btn');
-  console.log(buttonElement.innerText);
+function poop(season, id) {
+  const s = '.' + season;
+  const inText = addSpace(season) + ' v';
+  //console.log(s);
+  const buttonElement = document.querySelector(s);
+  //console.log(buttonElement.innerText);
   if (buttonElement.innerText.includes('>')) {
-    buttonElement.innerText = ('Season 1 v')
+    buttonElement.innerText = (`${inText}`);
+    //console.log("HHH");
+    //console.log(buttonElement.innerText);
     buttonElement.classList.add('active');
-    document.getElementById('dropD').style.display = 'block';
+    document.getElementById(id).style.display = 'block';
 
   } else {
-    buttonElement.innerText = ('Season 1 >')
+    const inTextClose = addSpace(season) + ' >';
+    buttonElement.innerText = (`${inTextClose}`)
     buttonElement.classList.remove('active');
-    document.getElementById('dropD').style.display = '';
+    document.getElementById(id).style.display = '';
 
   }
+}
 
-
-
+function addSpace(str) {
+  return str.split('n').join('n ');
 }
